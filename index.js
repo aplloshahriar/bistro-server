10e4fad70e11641c59974c0ec3b2dc862f5cf6cb
aplloshahriar <aaronshahriar.as@gmail.com>
2023-09-23 21:50:29 +0600

    verifyAdmin middleware applied

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// jwt verify
+// jwt verify middleware
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
@@ -61,9 +61,19 @@ async function run() {
       res.send({ token })
     })
 
+    // verifyAdmin middleware (verifyAdmin works after verifyJWT)
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if(user?.role!=='admin'){
+        return res.status(403).send({ error: true, message: 'forbidden access' });
+      }
+      next();
+    }
 
     // users related api
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyJWT,verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -79,6 +89,23 @@ async function run() {
         return res.send({ message: 'user already exists' });
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+    // admin 
+    // security layer verifyJWT
+    // email same
+    // check admin
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email != email) {
+        res.send({ admin: false })
+      }
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
       res.send(result);
     })
 
